2db1737c979cc15d5099548727acd5c64c17f969
Filip Homolka <fhomolka@protonmail.com>
2021-08-09 19:10:58 +0200

    C_Text now uses CB::Colour instead of SDL_Color

--- include/Components/C_Text.hpp
+++ include/Components/C_Text.hpp
@@ -8,17 +8,19 @@
 #include "AssetManager.hpp"
 #include "Game.hpp"
 #include "Types/Vector2.hpp"
+#include "Types/Colour.hpp"
 
 class C_Text: public Component {
 	private:
 		SDL_Rect position;
 		std::string text;
 		std::string fontFamily;
-		SDL_Color color;
+		//SDL_Color color;
+		CB::Colour colour;
 		SDL_Texture* texture;
 	public:
-		C_Text(int x, int y, std::string text, std::string fontFamily, const SDL_Color& color);
-		C_Text(CB::Vec2 newPosition, std::string text, std::string fontFamily, const SDL_Color& color);
+		C_Text(int x, int y, std::string text, std::string fontFamily, const CB::Colour& colour);
+		C_Text(CB::Vec2 newPosition, std::string text, std::string fontFamily, const CB::Colour& colour);
 
 		//TODO fhomolka 02/08/2021 23:30 -> Constructor that takes in transform, and a "bool follorTransform"
 		//									If it should follow transform, in Update, update own position to that of the transform
--- src/Components/C_Text.cpp
+++ src/Components/C_Text.cpp
@@ -1,21 +1,21 @@
 #include "Components/C_Text.hpp"
 
-C_Text::C_Text(int x, int y, std::string text, std::string fontFamily, const SDL_Color& color) {
+C_Text::C_Text(int x, int y, std::string text, std::string fontFamily, const CB::Colour& colour) {
 	this->position.x = x;
 	this->position.y = y;
 	this->text = text;
 	this->fontFamily = fontFamily;
-	this->color = color;
+	this->colour = colour;
 	SetLabelText(text, fontFamily);
 }
 
-C_Text::C_Text(CB::Vec2 newPosition, std::string text, std::string fontFamily, const SDL_Color& color) {
+C_Text::C_Text(CB::Vec2 newPosition, std::string text, std::string fontFamily, const CB::Colour& colour) {
 
 	this->position.x = newPosition.x;
 	this->position.y = newPosition.y;
 	this->text = text;
 	this->fontFamily = fontFamily;
-	this->color = color;
+	this->colour = colour;
 	SetLabelText(text, fontFamily);
 }
 
@@ -27,7 +27,7 @@ void C_Text::SetLabelText(std::string text, std::string newfontFamily) {
 	}
 
 	auto testFont = Game::assetManager->GetFont(fontFamily);
-	SDL_Surface* surface = TTF_RenderText_Blended(testFont, text.c_str(), this->color);
+	SDL_Surface* surface = TTF_RenderText_Blended(testFont, text.c_str(), this->colour.ToSDLColor());
 	texture = Game::renderer->CreateTextureFromSurface(surface);
 	Game::renderer->FreeSurface(surface);
 	Game::renderer->QueryTexture(texture, NULL, NULL, &position.w, &position.h);
