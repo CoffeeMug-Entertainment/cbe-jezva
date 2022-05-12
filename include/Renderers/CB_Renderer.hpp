3490a944464492de8695328ac3642358c67a9cb6
Fix <fhomolka@protonmail.com>
2022-05-12 20:11:37 +0200

    :rotating_light: Silence warnings about return values

--- include/Renderers/CB_Renderer.hpp
+++ include/Renderers/CB_Renderer.hpp
@@ -10,12 +10,12 @@ public:
 	CB_Renderer(){};
 	~CB_Renderer(){};
 	
-	virtual bool Initialize(){};
+	virtual bool Initialize(){return false;};
 	virtual void Render(){};
 
 	virtual void RenderCopy(SDL_Texture* texture, SDL_Rect position){};
 	virtual void RenderCopyEx(SDL_Texture* texture, SDL_Rect* srcRect, SDL_Rect* dstRect, double angle, SDL_Point* center, SDL_RendererFlip flip){};
-	virtual SDL_Texture* CreateTextureFromSurface(SDL_Surface *surface){};
+	virtual SDL_Texture* CreateTextureFromSurface(SDL_Surface *surface){ return nullptr;};
 	virtual void QueryTexture(SDL_Texture* texture, uint32_t* format, int* access, int* w, int* h){};
 
 	virtual void SetRenderDrawColor(uint8_t r, uint8_t g, uint8_t b, uint8_t a){};
