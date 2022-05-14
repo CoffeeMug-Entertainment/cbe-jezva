593fa445784e1503a5bb57335860f5e8f8b4affa
Fix <fhomolka@protonmail.com>
2022-05-14 11:39:44 +0200

    :recycle: AssetManager stores sdl_stb_font_cache instead of the TTF_Font given by SDL_ttf

--- include/AssetManager.hpp
+++ include/AssetManager.hpp
@@ -3,11 +3,11 @@
 
 #include "EntityManager.hpp"
 #include "TextureManager.hpp"
-#include "FontManager.hpp"
+//#include "FontManager.hpp"
+#include "sdlStbFont.h"
 #include "nlohmann/json.hpp"
 //#include <Data/Tilemap.hpp>
 
-#include <SDL2/SDL_ttf.h>
 #include <map>
 #include <string>
 
@@ -20,14 +20,14 @@ class AssetManager {
         void AddTexture(std::string textureId, const char* filePath);
 		SDL_Texture* GetTexture(std::string textureId);
 		void AddFont(std::string fontId, const char* filePath, int fontSize);
-		TTF_Font* GetFont(std::string fontId);
+		sdl_stb_font_cache* GetFont(std::string fontId);
         void LoadFromAssetsJson(const char* filePath);
         //Tilemap* CreateTilemap(std::string textureId, std::string tilemapId, int tileSize, int margin = 0);
         //Tilemap* GetTilemap(std::string tilemapId);
     private:
         EntityManager* entityManager;
         std::map<std::string, SDL_Texture*> textures;
-        std::map<std::string, TTF_Font*> fonts;
+        std::map<std::string, sdl_stb_font_cache*> fonts;
         //std::map<std::string, CB::Tilemap> tilemaps;
 
 };
