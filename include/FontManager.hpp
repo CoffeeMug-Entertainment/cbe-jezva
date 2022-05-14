ce13489c67e09d79ac914c156958d192805f0820
Fix <fhomolka@protonmail.com>
2022-05-14 11:39:02 +0200

    :recycle: FontManager now uses sdl_stb_font instead of SDL_ttf

--- include/FontManager.hpp
+++ include/FontManager.hpp
@@ -2,12 +2,13 @@
 #define FONTMANAGER_HPP
 
 #include "Game.hpp"
-#include <SDL2/SDL_ttf.h>
+#include "sdlStbFont.h"
+#include "Types/Vector2.hpp"
 
 class FontManager {
    public:
-		static TTF_Font* LoadFont(const char* fileName, int fontSize);
-		static void Draw(SDL_Texture* texture, SDL_Rect position);
+		static sdl_stb_font_cache* LoadFont(const char* fileName, int fontSize);
+		static void Draw(sdl_stb_font_cache* font_cache, CB::Vec2 position, std::string text);
 };
 
 
--- src/FontManager.cpp
+++ src/FontManager.cpp
@@ -1,11 +1,31 @@
 #include "FontManager.hpp"
 #include "Game.hpp"
+#include "Renderers/Ren_SDL.hpp"
 
-TTF_Font* FontManager::LoadFont(const char* fileName, int fontSize){
-    return TTF_OpenFont(fileName, fontSize);
+#define STB_TRUETYPE_IMPLEMENTATION
+#include "stb_truetype.h"
+#define STB_TRUETYPE_INCLUDE_HANDLED
+#define SDL_STB_FONT_IMPL
+#include "sdlStbFont.h"
+
+sdl_stb_font_cache* FontManager::LoadFont(const char* fileName, int fontSize){
+
+    std::ifstream fontFile(fileName);
+    std::ostringstream ss;
+    std::string fontData;
+
+    ss << fontFile.rdbuf();
+    fontData = ss.str();
+
+    sdl_stb_font_cache* font_cache = new sdl_stb_font_cache();
+    font_cache->faceSize = fontSize;
+    font_cache->loadFont(fontData.c_str());
+    SDL_Renderer* ren = static_cast<Ren_SDL*>(Game::renderer)->Renderer();
+    font_cache->bindRenderer(ren);
+
+    return font_cache;
 }
 
-void FontManager::Draw(SDL_Texture* texture, SDL_Rect position){
-    //SDL_RenderCopy(Game::renderer, texture, NULL, &position);
-    Game::renderer->RenderCopy(texture, position);
+void FontManager::Draw(sdl_stb_font_cache* font_cache, CB::Vec2 position, std::string text){
+    font_cache->drawText(static_cast<int>(position.x), static_cast<int>(position.y), text);
 }
