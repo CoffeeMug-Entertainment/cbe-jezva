bd339dc210c63d2ebbf145ab3cf2cae083778d97
Fix <fhomolka@protonmail.com>
2022-05-17 13:50:14 +0200

    :bug: This is required by SDL2, for Windows, ever since 7

--- include/AudioManager.hpp
+++ include/AudioManager.hpp
@@ -2,6 +2,9 @@
 #define AUDIOMANAGER_HPP
 
 #include "SDL2/SDL_audio.h"
+#ifdef _WIN32 //See Simple-SDL2-Audio's README.md
+#define SDL_AUDIO_ALLOW_CHANGES SDL_AUDIO_ALLOW_FREQUENCY_CHANGE | SDL_AUDIO_ALLOW_CHANNELS_CHANGE
+#endif
 #include "audio.h"
 
 class AudioManager
