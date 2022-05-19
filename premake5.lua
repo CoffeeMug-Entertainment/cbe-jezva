9e18e2134e13079e77f49a61a38ac4b77456e05a
Fix <fhomolka@protonmail.com>
2022-05-19 16:53:32 +0200

    :recycle: Some premake cleanup

--- premake5.lua
+++ premake5.lua
@@ -7,16 +7,19 @@ project "CoffeeBean"
 	--targetdir = ("lib/%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}/%{prj.name}")
 	--objdir = ("obj/%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}/%{prj.name}")
 
-	includedirs
+	sysincludedirs
 	{
-		"include",
-		--"thirdparty/SDL2/include",
-		--"thirdpartySDL2_ttf",
+		"thirdparty/SDL2/include",
 		"thirdparty/json/single_include",
 		"thirdparty/Xoshiro/",
 		"thirdparty/stb",
 		"thirdparty/sdl_stb_font",
 		"thirdparty/Simple-SDL2-Audio/src"
+	}
+
+	includedirs
+	{
+		"include"
 	}
 
 	files
@@ -31,12 +34,14 @@ project "CoffeeBean"
 		"SDL2",
 	}
 
-	filter "system:windows"
-		staticruntime "on"
-		includedirs
+	filter {"system:linux"}
+		defines
 		{
-			"thirdparty/SDL2/include",
+			"CBE_LINUX"
 		}
+
+	filter {"system:windows"}
+		staticruntime "on"
 
 		buildoptions
 		{
@@ -61,14 +66,25 @@ project "CoffeeBean"
 
 		defines
 		{
-			"_WIN32"
+			"CBE_WINDOWS",
+			"_WIN32",
 		}
 		
 	filter "configurations:Debug"
 		 runtime "Debug"
 		 symbols "on"
 
+		 defines
+		 {
+			 "CBE_DEBUG"
+		 }
+
 	 filter "configurations:Release"
 		 runtime "Release"
 		 optimize "on"
+
+		 defines
+		 {
+			 "CBE_RELEASE"
+		 }
 
