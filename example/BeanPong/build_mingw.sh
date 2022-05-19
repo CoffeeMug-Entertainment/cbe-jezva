a6a73663dd9c7fc4206af741087bff5df54a12c6
Fix <fhomolka@protonmail.com>
2022-05-19 21:29:06 +0200

    :sparkles: add a build script for mingw

---
+++ example/BeanPong/build_mingw.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+
+# CMake cannot add parent directiories, so this is a dirty hack
+# We copy CBE into this folder, then delete it
+if [[ ! -f ./thirdparty ]]
+then
+    mkdir -pv thirdparty/CoffeeBean
+    cp -r ../../include thirdparty/CoffeeBean/include
+    cp -r ../../src thirdparty/CoffeeBean/src
+    cp -r ../../thirdparty thirdparty/CoffeeBean/thirdparty
+    cp ../../CMakeLists.txt thirdparty/CoffeeBean/CMakeLists.txt
+fi
+
+mkdir -pv build
+cmake -S . -B build -G Ninja
+cd build
+ninja
+cd ..
